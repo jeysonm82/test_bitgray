c518581fcf815814418e6e322e3bd226c3e4733b
jeysonmc <jeyson.molina.co@gmail.com>
2016-09-01 10:34:00 -0500

    Fixes #2 CRUD admins for models.

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from models import Clientes, Productos, Sedes, Compras, Log
 
-# Register your models here.
+admin.site.register(Clientes)
+admin.site.register(Productos)
+admin.site.register(Sedes)
+admin.site.register(Log)
+
+class ComprasAdmin(admin.ModelAdmin):
+    model = Compras
+    list_display=('cliente', 'producto', 'sede', 'precio', 'descripcion', 'fecha')
+
+admin.site.register(Compras, ComprasAdmin)
